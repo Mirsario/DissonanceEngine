9c788bbcd519e84341c00689ba4d1f0d7c81735f
Mirsario <me@mirsar.io>
2019-10-06 04:31:49 +0300

    Fix PostProcessPass.cs after previous commits making it render per-camera.

--- Projects/GameEngine/Graphics/Rendering/RenderPasses/PostProcessPass.cs
+++ Projects/GameEngine/Graphics/Rendering/RenderPasses/PostProcessPass.cs
@@ -9,15 +9,19 @@ namespace GameEngine.Graphics
 		{
 			Framebuffer.BindWithDrawBuffers(framebuffer);
 
+			GL.Enable(EnableCap.Blend);
+
 			Shader.SetShader(passShader);
 
 			passShader.SetupCommonUniforms();
+
+			GL.Viewport(0,0,Screen.Width,Screen.Height);
 
 			for(int i = 0;i<Rendering.cameraList.Count;i++) {
 				var camera = Rendering.cameraList[i];
 
 				var viewport = GetViewport(camera);
-				GL.Viewport(viewport.x,viewport.y,viewport.width,viewport.height);
+				//GL.Viewport(viewport.x,viewport.y,viewport.width,viewport.height);
 
 				passShader.SetupCameraUniforms(camera,camera.Transform.Position);
 
@@ -34,17 +38,25 @@ namespace GameEngine.Graphics
 					}
 				}
 
+				var vpPointsA = new Vector4(
+					viewport.x/(float)Screen.width,
+					viewport.y/(float)Screen.height,
+					viewport.Right/(float)Screen.width,
+					viewport.Bottom/(float)Screen.height
+				);
+				var vpPointsB = (vpPointsA*2f)-Vector4.One;
+
 				GL.Begin(PrimitiveTypeGL.Quads);
-				GL.Vertex2(-1f,1f);
-				GL.TexCoord2(0f,1f);
-				GL.Vertex2(-1f,-1f);
-				GL.TexCoord2(0f,0f);
-				GL.Vertex2(1f,-1f);
-				GL.TexCoord2(1f,0f);
-				GL.Vertex2(1f,1f);
-				GL.TexCoord2(1f,1f);
+
+				GL.Vertex2(vpPointsB.x,vpPointsB.w); GL.TexCoord2(vpPointsA.x,vpPointsA.w);
+				GL.Vertex2(vpPointsB.x,vpPointsB.y); GL.TexCoord2(vpPointsA.x,vpPointsA.y);
+				GL.Vertex2(vpPointsB.z,vpPointsB.y); GL.TexCoord2(vpPointsA.z,vpPointsA.y);
+				GL.Vertex2(vpPointsB.z,vpPointsB.w); GL.TexCoord2(vpPointsA.z,vpPointsA.w);
+
 				GL.End();
 			}
+
+			GL.Disable(EnableCap.Blend);
 		}
 	}
 }
