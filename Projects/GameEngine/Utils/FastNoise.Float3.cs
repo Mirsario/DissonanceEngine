68c0a1d276e147f6b4f5401207011d1748e29942
Mirsario <me@mirsar.io>
2019-10-18 17:40:46 +0300

    Replaced the very much terrible PerlinNoiseFloat & PerlinNoiseDouble classes with the very much great FastNoise library by Jordan Peck.

---
+++ Projects/GameEngine/Utils/FastNoise.Float3.cs
@@ -0,0 +1,22 @@
+﻿//Based on https://github.com/Auburns/FastNoise_CSharp/blob/master/FastNoise.cs under MIT License
+//Copyright(c) 2017 Jordan Peck
+
+using DECIMAL = System.Single;//using FN_DECIMAL = System.Double;
+
+namespace GameEngine.Utils
+{
+	public partial class FastNoise
+	{
+		private struct Float3
+		{
+			public readonly DECIMAL x, y, z;
+
+			public Float3(DECIMAL x,DECIMAL y,DECIMAL z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+		}
+	}
+}
