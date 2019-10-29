7f73b2b5f4bc6e431e2598f92321bbea66b7abab
Mirsario <me@mirsar.io>
2019-10-29 15:35:22 +0300

    Add SourceRectangle property to Sprite.

--- Projects/GameEngine/Graphics/Components/Sprite.cs
+++ Projects/GameEngine/Graphics/Components/Sprite.cs
@@ -1,4 +1,6 @@
+using OpenTK.Graphics.OpenGL;
 using GameEngine.Graphics;
+using PrimitiveType = OpenTK.Graphics.OpenGL.PrimitiveType;
 
 namespace GameEngine
 {
@@ -10,9 +12,23 @@ namespace GameEngine
 			FlipVertically = 2
 		}
 
+		protected static readonly Bounds DefaultBounds = new Bounds(Vector3.Zero,Vector3.One);
+
 		public SpriteEffects spriteEffects;
 
+		protected bool useMesh = true;
+		protected RectFloat sourceRectangle = RectFloat.Default;
+		protected Vector4 sourceUV = new Vector4(0f,0f,1f,1f);
 		protected Material material;
+
+		public RectFloat SourceRectangle {
+			get => sourceRectangle;
+			set {
+				sourceRectangle = value;
+				sourceUV = new Vector4(value.x,value.y,value.Right,value.Bottom);
+				useMesh = value.x==0f && value.y==0f && value.width==1f && value.height==1f;
+			}
+		}
 
 		public override Material Material {
 			get => material;
@@ -21,19 +37,48 @@ namespace GameEngine
 
 		public override bool GetRenderData(Vector3 rendererPosition,Vector3 cameraPosition,out Material material,out Bounds bounds,out object renderObject)
 		{
-			var mesh = spriteEffects switch {
-				SpriteEffects.FlipHorizontally|SpriteEffects.FlipVertically => PrimitiveMeshes.quadXYFlipped,
-				SpriteEffects.FlipHorizontally => PrimitiveMeshes.quadXFlipped,
-				SpriteEffects.FlipVertically => PrimitiveMeshes.quadXFlipped,
-				_ => PrimitiveMeshes.quad
-			};
-
 			material = this.material;
-			bounds = mesh.bounds;
-			renderObject = mesh;
+
+			if(useMesh) {
+				var mesh = spriteEffects switch {
+					SpriteEffects.FlipHorizontally|SpriteEffects.FlipVertically => PrimitiveMeshes.quadXYFlipped,
+					SpriteEffects.FlipHorizontally => PrimitiveMeshes.quadXFlipped,
+					SpriteEffects.FlipVertically => PrimitiveMeshes.quadXFlipped,
+					_ => PrimitiveMeshes.quad
+				};
+
+				bounds = mesh.bounds;
+				renderObject = mesh;
+			} else {
+				bounds = DefaultBounds;
+				renderObject = null;
+			}
 
 			return true;
 		}
-		public override void Render(object renderObject) => ((Mesh)renderObject).DrawMesh();
+		public override void Render(object renderObject)
+		{
+			if(useMesh) {
+				((Mesh)renderObject).DrawMesh();
+			} else {
+				int uvAttrib = (int)AttributeId.Uv0;
+
+				Vector4 uvPoints = spriteEffects switch {
+					SpriteEffects.FlipHorizontally|SpriteEffects.FlipVertically => new Vector4(sourceUV.z,sourceUV.w,sourceUV.x,sourceUV.y),
+					SpriteEffects.FlipHorizontally => new Vector4(sourceUV.z,sourceUV.y,sourceUV.x,sourceUV.w),
+					SpriteEffects.FlipVertically => new Vector4(sourceUV.x,sourceUV.w,sourceUV.z,sourceUV.y),
+					_ => sourceUV
+				};
+
+				GL.Begin(PrimitiveType.Quads);
+
+				GL.VertexAttrib2(uvAttrib,uvPoints.x,uvPoints.w); GL.Vertex2(-0.5f,-0.5f);
+				GL.VertexAttrib2(uvAttrib,uvPoints.x,uvPoints.y); GL.Vertex2(-0.5f, 0.5f);
+				GL.VertexAttrib2(uvAttrib,uvPoints.z,uvPoints.y); GL.Vertex2( 0.5f, 0.5f);
+				GL.VertexAttrib2(uvAttrib,uvPoints.z,uvPoints.w); GL.Vertex2( 0.5f,-0.5f);
+
+				GL.End();
+			}
+		}
 	}
 }
