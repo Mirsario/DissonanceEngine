bbb476634fd5bcf635799204d05e0f113d6ccf9d
Mirsario <me@mirsar.io>
2020-02-29 15:35:44 +0300

    Basic glTF geometry support.

---
+++ Src/IO/Graphics/Models/GltfManager.Enums.cs
@@ -0,0 +1,36 @@
+namespace Dissonance.Engine.IO.Graphics.Models
+{
+	partial class GltfManager
+	{
+		public enum ComponentType
+		{
+			SByte = 5120,
+			Byte = 5121,
+			Short = 5122,
+			UShort = 5123,
+			UInt = 5125,
+			Float = 5126
+		}
+
+		public enum InterpolationAlgorithm
+		{
+			Linear,
+			Step,
+			CubicSpline
+		}
+
+		public enum BufferTarget
+		{
+			/// <summary> Vertex attributes. </summary>
+			ArrayBuffer = 34962,
+			/// <summary> Vertex array indices. </summary>
+			ElementArrayBuffer = 34963,
+		}
+
+		public enum CameraType
+		{
+			Perspective,
+			Ortographic
+		}
+	}
+}
