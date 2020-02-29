b86fc53b3d65ac624718a96d121726e54b38142d
Mirsario <me@mirsar.io>
2020-02-29 14:53:35 +0300

    Added stride field to CustomVertexAttributes

--- Src/Graphics/Objects/VertexAttributes/CustomVertexAttribute.cs
+++ Src/Graphics/Objects/VertexAttributes/CustomVertexAttribute.cs
@@ -17,13 +17,18 @@ namespace Dissonance.Engine.Graphics
 
 		internal static CustomVertexAttribute[] instances;
 
+		private static Dictionary<Type,int> idByType;
+
+		public static int Count { get; private set; }
+
 		public string NameId { get; protected set; }
 		public VertexAttribPointerType PointerType { get; protected set; }
 		public bool IsNormalized { get; protected set; }
 		public int Size { get; protected set; }
+		public int Stride { get; protected set; }
 		public int Offset { get; protected set; }
 
-		public static int Count { get; private set; }
+		public abstract Type BufferType { get; }
 
 		internal CustomVertexAttribute() { }
 
@@ -32,6 +37,8 @@ namespace Dissonance.Engine.Graphics
 			if(CustomVertexBuffer.TypeById==null) {
 				throw new InvalidOperationException($"{nameof(CustomVertexAttribute)}.{nameof(Initialize)}() must be called after {nameof(CustomVertexBuffer)}.{nameof(CustomVertexAttribute.Initialize)}().");
 			}
+
+			idByType = new Dictionary<Type,int>();
 
 			var instanceList = new List<CustomVertexAttribute>();
 			var bufferAttachmentsList = new List<int>[CustomVertexBuffer.Count];
@@ -49,6 +56,8 @@ namespace Dissonance.Engine.Graphics
 					.GetField(nameof(IDs<CustomVertexAttribute>.id),BindingFlags.Public|BindingFlags.Static)
 					.SetValue(null,id);
 
+				idByType[type] = id;
+
 				//Add this vertex attribute's id to its vertex buffer's attribute attachments list.
 				var vertexBufferType = type.BaseType.GetGenericArguments()[0];
 				int vertexBufferId = CustomVertexBuffer.GetId(vertexBufferType);
@@ -64,25 +73,30 @@ namespace Dissonance.Engine.Graphics
 		}
 
 		public static int GetId<T>() where T : CustomVertexAttribute => IDs<T>.id;
+		public static int GetId(Type type) => idByType[type];
 		public static T GetInstance<T>() where T : CustomVertexAttribute => (T)instances[IDs<T>.id];
+		public static CustomVertexAttribute GetInstance(Type type) => instances[idByType[type]];
 		public static CustomVertexAttribute GetInstance(int id) => instances[id];
 	}
 
 	public abstract class CustomVertexAttribute<TBuffer> : CustomVertexAttribute where TBuffer : CustomVertexBuffer, new()
 	{
+		public override Type BufferType => typeof(TBuffer);
+
 		protected CustomVertexAttribute() : base()
 		{
 			//This is quite weird.
 
-			Init(out var nameId,out var pointerType,out var isNormalized,out var size,out var offset);
+			Init(out var nameId,out var pointerType,out var isNormalized,out var size,out var stride,out var offset);
 
 			NameId = nameId;
 			PointerType = pointerType;
 			IsNormalized = isNormalized;
 			Size = size;
+			Stride = stride;
 			Offset = offset;
 		}
 
-		public abstract void Init(out string nameId,out VertexAttribPointerType pointerType,out bool isNormalized,out int size,out int offset);
+		public abstract void Init(out string nameId,out VertexAttribPointerType pointerType,out bool isNormalized,out int size,out int stride,out int offset);
 	}
 }
