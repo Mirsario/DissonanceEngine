ff9c1bd85ca5ac6231e388f61980cf58b9e72750
Mirsario <me@mirsar.io>
2020-03-01 16:38:14 +0300

    Added MeshBuffer.SetData functions, although the current design is not very satisfying.

--- Src/Graphics/Objects/MeshBuffers/MeshBuffer.cs
+++ Src/Graphics/Objects/MeshBuffers/MeshBuffer.cs
@@ -5,7 +5,17 @@ using System.Runtime.InteropServices;
 
 namespace Dissonance.Engine.Graphics
 {
-	public abstract class MeshBuffer : IDisposable
+	public interface IMeshBuffer : IDisposable
+	{
+		void SetData(byte[] byteData);
+	}
+
+	public interface IMeshBuffer<TData> : IMeshBuffer
+	{
+		void SetData<TProvidedData>(byte[] byteData,Func<TProvidedData,TData> cast) where TProvidedData : unmanaged;
+	}
+
+	public abstract class MeshBuffer : IMeshBuffer
 	{
 		public Mesh mesh;
 
@@ -18,22 +28,7 @@ namespace Dissonance.Engine.Graphics
 
 		protected unsafe static void SetDataHelper<T>(ref T[] data,byte[] byteData) where T : unmanaged
 		{
-			if(byteData==null) {
-				data = null;
-				return;
-			}
-
-			int tSize = Marshal.SizeOf<T>();
-
-			if(byteData.Length%tSize!=0) {
-				throw new ArgumentException($"Data array's length {byteData.Length} must be dividable by size of {typeof(T).Name}, which is {tSize}.");
-			}
-
-			int newLength = byteData.Length/tSize;
-
-			data = new T[newLength];
-
-			if(byteData.Length==0) {
+			if(!PrepareSetData<T,T>(ref data,byteData)) {
 				return;
 			}
 
@@ -44,6 +39,44 @@ namespace Dissonance.Engine.Graphics
 					data[i] = tPtr[i];
 				}
 			}
+		}
+		protected unsafe static void SetDataHelper<TLocalData,TProvidedData>(ref TLocalData[] data,byte[] byteData,Func<TProvidedData,TLocalData> cast)
+			where TLocalData : unmanaged
+			where TProvidedData : unmanaged
+		{
+			if(!PrepareSetData<TLocalData,TProvidedData>(ref data,byteData)) {
+				return;
+			}
+
+			fixed(byte* bytePtr = byteData) {
+				var tPtr = (TProvidedData*)bytePtr;
+
+				for(int i = 0;i<data.Length;i++) {
+					data[i] = cast(tPtr[i]);
+				}
+			}
+		}
+
+		private static bool PrepareSetData<TLocalData,TProvidedData>(ref TLocalData[] data,byte[] byteData)
+			where TLocalData : unmanaged
+			where TProvidedData : unmanaged
+		{
+			if(byteData==null) {
+				data = null;
+				return false;
+			}
+
+			int tSize = Marshal.SizeOf<TProvidedData>();
+
+			if(byteData.Length%tSize!=0) {
+				throw new ArgumentException($"Data array's length {byteData.Length} must be dividable by size of {typeof(TProvidedData).Name}, which is {tSize}.");
+			}
+
+			int newLength = byteData.Length/tSize;
+
+			data = new TLocalData[newLength];
+
+			return byteData.Length!=0;
 		}
 	}
 }
