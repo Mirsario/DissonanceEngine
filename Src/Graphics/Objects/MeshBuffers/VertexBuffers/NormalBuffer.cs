e249517acee4c7eae63f840b04befd4768818f5c
Mirsario <me@mirsar.io>
2020-03-06 22:55:48 +0300

    Added NormalBuffer recalculation for non-indexed triangle meshes.

--- Src/Graphics/Objects/MeshBuffers/VertexBuffers/NormalBuffer.cs
+++ Src/Graphics/Objects/MeshBuffers/VertexBuffers/NormalBuffer.cs
@@ -1,18 +1,39 @@
-﻿namespace Dissonance.Engine.Graphics
+﻿using System;
+using Dissonance.Framework.Graphics;
+
+namespace Dissonance.Engine.Graphics
 {
 	public class NormalBuffer : CustomVertexBuffer<Vector3>
 	{
 		public void Recalculate()
 		{
-			var vertices = mesh.Vertices;
+			if(mesh.PrimitiveType!=PrimitiveType.Triangles) {
+				throw new InvalidOperationException($"{nameof(Recalculate)}() can only work on meshes with {nameof(PrimitiveType.Triangles)} {nameof(Mesh.PrimitiveType)}.");
+			}
+
+			var vertices = mesh.Vertices ?? throw new InvalidOperationException($"{nameof(Recalculate)}() requires {nameof(VertexBuffer)} to be ready.");
 			var triangles = mesh.Triangles;
+			bool isIndexed = triangles!=null;
+			int indexCount = isIndexed ? triangles.Length : vertices.Length;
+
+			if(!isIndexed && indexCount%3!=0) {
+				throw new InvalidOperationException($"{nameof(Recalculate)}() requires vertex count to be divisable by 3, if no indices are provided.");
+			}
 
 			var newNormals = new Vector3[vertices.Length];
 
-			for(uint i = 0;i<triangles.Length;i+=3) {
-				uint i1 = triangles[i];
-				uint i2 = triangles[i+1];
-				uint i3 = triangles[i+2];
+			for(uint i = 0;i<indexCount;i+=3) {
+				uint i1,i2,i3;
+
+				if(isIndexed) {
+					i1 = triangles[i];
+					i2 = triangles[i+1];
+					i3 = triangles[i+2];
+				} else {
+					i1 = i;
+					i2 = i+1;
+					i3 = i+2;
+				}
 
 				var v1 = vertices[i1];
 				var v2 = vertices[i2];
