2d1136ed352cc0e02f27b16823fd220ba334239b
Mirsario <me@mirsar.io>
2020-03-07 03:08:14 +0300

    Commented out previous 2 commits.

--- Src/Physics/Components/Colliders/Collider.cs
+++ Src/Physics/Components/Colliders/Collider.cs
@@ -52,9 +52,9 @@ namespace Dissonance.Engine.Physics
 		{
 			base.OnDispose();
 
-			if(OwnsShape) {
+			/*if(OwnsShape) {
 				collShape?.Dispose();
-			}
+			}*/
 		}
 
 		protected void TryUpdateCollider()
