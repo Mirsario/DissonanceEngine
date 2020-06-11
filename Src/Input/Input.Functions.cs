3ce2e3821a23877efe16ed1fa0291555d3697c12
Mirsario <me@mirsar.io>
2020-06-11 07:34:52 +0300

    Added Input.GetTrigger<T>().

--- Src/Input/Input.Functions.cs
+++ Src/Input/Input.Functions.cs
@@ -5,19 +5,14 @@ namespace Dissonance.Engine
 	partial class Input
 	{
 		//Mouse
-
 		public static bool GetMouseButton(MouseButton button) => CurrentInput.mouseButtons[(int)button];
 		public static bool GetMouseButtonDown(MouseButton button) => CurrentInput.mouseButtons[(int)button] && !PrevInput.mouseButtons[(int)button];
 		public static bool GetMouseButtonUp(MouseButton button) => !CurrentInput.mouseButtons[(int)button] && PrevInput.mouseButtons[(int)button];
-
 		//Keys
-
 		public static bool GetKey(Keys key) => CurrentInput.pressedKeys.ContainsKey(key);
 		public static bool GetKeyDown(Keys key) => CurrentInput.pressedKeys.ContainsKey(key) && !PrevInput.pressedKeys.ContainsKey(key);
 		public static bool GetKeyUp(Keys key) => !CurrentInput.pressedKeys.ContainsKey(key) && PrevInput.pressedKeys.ContainsKey(key);
-
 		//Directions
-
 		public static float GetDirection(Keys negative,Keys positive) => (GetKey(negative) ? -1f : 0f)+(GetKey(positive) ? 1f : 0f);
 		public static Vector2 GetDirection(Keys up,Keys down,Keys left,Keys right) => new Vector2(
 			(GetKey(right) ? 1f : 0f)-(GetKey(left) ? 1f : 0f),
