ee8245d5b00609327548549c26ed8193c514011b
Mirsario <me@mirsar.io>
2020-06-18 15:57:26 +0300

    Removed 'readonly' from from SingletonInputTrigger for threading reasons.

--- Src/Input/Input.Triggers.cs
+++ Src/Input/Input.Triggers.cs
@@ -11,7 +11,7 @@ namespace Dissonance.Engine
 		public static InputTrigger RegisterTrigger(string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
 			=> RegisterTrigger(typeof(InputTrigger),name,bindings,minValue,maxValue);
 		public static T GetTrigger<T>() where T : SingletonInputTrigger
-			=> (T)triggers[SingletonInputTrigger.Info<T>.Id];
+			=> (T)triggers[SingletonInputTrigger.Info<T>.id];
 
 		internal static InputTrigger RegisterTrigger(Type type,string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
 		{
