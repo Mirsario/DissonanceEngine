39e3768e04b4557f67d4ed6eb9b38826b1cf8b37
Mirsario <me@mirsar.io>
2020-07-14 20:39:26 +0300

    LinqExtensions cleanup.

--- Src/Utils/Extensions/LinqExtensions.cs
+++ Src/Utils/Extensions/LinqExtensions.cs
@@ -11,47 +11,37 @@ namespace Dissonance.Engine.Utils.Extensions
 				to.Add(pair.Key,pair.Value);
 			}
 		}
-
 		public static IEnumerable<TResult> SelectIgnoreNull<TSource, TResult>(this IEnumerable<TSource> source,Func<TSource,TResult> selector)
 		{
-			if(source==null) { throw new ArgumentNullException(nameof(source)); }
-			if(selector==null) { throw new ArgumentNullException(nameof(selector)); }
-			return SelectIgnoreNullIterator(source,selector);
-		}
-		private static IEnumerable<TResult> SelectIgnoreNullIterator<TSource, TResult>(IEnumerable<TSource> source,Func<TSource,TResult> selector)
-		{
+			if(source==null) {
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if(selector==null) {
+				throw new ArgumentNullException(nameof(selector));
+			}
+
 			foreach(var element in source) {
 				var result = selector(element);
+
 				if(result!=null) {
 					yield return result;
 				}
 			}
 		}
-		public static bool TryFirst<T>(this IEnumerable<T> array,Func<T,bool> func,out T result)
+		public static bool TryGetFirst<T>(this IEnumerable<T> array,Func<T,bool> func,out T result)
 		{
 			foreach(var val in array) {
 				if(func(val)) {
 					result = val;
+
 					return true;
 				}
 			}
-			result = default;
-			return false;
-		}
 
-		public static string AllToString<T>(this T[] array,string separator = ",")
-		{
-			if(array==null) {
-				return "NULL";
-			}
-			string str = "";
-			for(int i = 0;i<array.Length;i++) {
-				str += array[i].ToString();
-				if(i<array.Length-1) {
-					str += separator;
-				}
-			}
-			return str;
+			result = default;
+
+			return false;
 		}
 	}
 }
