fb8993fe13cbda6d8f8e74c5a62a833b94eafe7d
Mirsario <me@mirsar.io>
2020-07-24 00:15:08 +0300

    Made InputEngine safe for multiinstance mode.

--- Src/Input/InputEngine.Signals.cs
+++ Src/Input/InputEngine.Signals.cs
@@ -6,38 +6,13 @@ namespace Dissonance.Engine.Input
 {
 	partial class InputEngine
 	{
-		private static Dictionary<string,int> signalIdByName;
-		private static List<(Func<object,float> getter,object arg)> signals;
+		private Dictionary<string,int> signalIdByName;
+		private List<(Func<object,float> getter,object arg)> signals;
 
-		public static void RegisterSignal(string nameId,Func<object,float> valueGetter,object arg = null)
-		{
-			var tuple = (valueGetter,arg);
-
-			if(signalIdByName.TryGetValue(nameId,out int id)) {
-				signals[id] = tuple;
-
-				return;
-			}
-
-			signalIdByName[nameId] = signals.Count;
-
-			signals.Add(tuple);
-		}
-		public static float GetSignal(string nameId)
-		{
-			if(!signalIdByName.TryGetValue(nameId,out int id)) {
-				return 0f;
-			}
-
-			var (getter,arg) = signals[id];
-
-			return getter(arg);
-		}
-
-		private static void InitSignals()
+		private void InitSignals()
 		{
 			signalIdByName = new Dictionary<string,int>(StringComparer.InvariantCultureIgnoreCase);
-			signals = new List<(Func<object,float>,object)>();
+			signals = new List<(Func<object,float>, object)>();
 
 			//Register key signals
 			foreach(Keys key in Enum.GetValues(typeof(Keys))) {
@@ -52,6 +27,34 @@ namespace Dissonance.Engine.Input
 			RegisterSignal("MouseX",arg => MouseDelta.x);
 			RegisterSignal("MouseY",arg => MouseDelta.y);
 			RegisterSignal("MouseWheel",arg => MouseWheel);
+		}
+
+		public static void RegisterSignal(string nameId,Func<object,float> valueGetter,object arg = null)
+		{
+			var tuple = (valueGetter,arg);
+			var instance = Instance;
+
+			if(instance.signalIdByName.TryGetValue(nameId,out int id)) {
+				instance.signals[id] = tuple;
+
+				return;
+			}
+
+			instance.signalIdByName[nameId] = instance.signals.Count;
+
+			instance.signals.Add(tuple);
+		}
+		public static float GetSignal(string nameId)
+		{
+			var instance = Instance;
+
+			if(!instance.signalIdByName.TryGetValue(nameId,out int id)) {
+				return 0f;
+			}
+
+			var (getter,arg) = instance.signals[id];
+
+			return getter(arg);
 		}
 	}
 }
--- Src/Input/InputEngine.Triggers.cs
+++ Src/Input/InputEngine.Triggers.cs
@@ -5,43 +5,17 @@ namespace Dissonance.Engine.Input
 {
 	partial class InputEngine
 	{
-		private static InputTrigger[] triggers;
-		private static Dictionary<string,InputTrigger> triggersByName;
+		private InputTrigger[] triggers;
+		private Dictionary<string,InputTrigger> triggersByName;
 
-		public static InputTrigger RegisterTrigger(string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
-			=> RegisterTrigger(typeof(InputTrigger),name,bindings,minValue,maxValue);
-		public static T GetTrigger<T>() where T : SingletonInputTrigger
-			=> (T)triggers[SingletonInputTrigger.Info<T>.id];
+		public static InputTrigger[] Triggers => Instance.triggers;
 
-		internal static InputTrigger RegisterTrigger(Type type,string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
-		{
-			if(triggersByName.TryGetValue(name,out var trigger)) {
-				trigger.Bindings = bindings;
-				return trigger;
-			}
-
-			int id = triggers.Length;
-
-			trigger = (InputTrigger)Activator.CreateInstance(type,true); //new InputTrigger();
-
-			trigger.Init(id,name,bindings,minValue ?? InputTrigger.DefaultMinValue,maxValue ?? InputTrigger.DefaultMaxValue);
-
-			Array.Resize(ref triggers,id+1);
-
-			triggers[id] = trigger;
-			triggersByName[name] = trigger;
-
-			InputTrigger.Count = triggers.Length;
-
-			return trigger;
-		}
-
-		private static void InitTriggers()
+		private void InitTriggers()
 		{
 			triggers = new InputTrigger[0];
 			triggersByName = new Dictionary<string,InputTrigger>();
 		}
-		private static void UpdateTriggers()
+		private void UpdateTriggers()
 		{
 			for(int i = 0;i<triggers.Length;i++) {
 				var trigger = triggers[i];
@@ -56,6 +30,36 @@ namespace Dissonance.Engine.Input
 					trigger.Value += trigger.bindings[j].Value;
 				}
 			}
+		}
+
+		public static InputTrigger RegisterTrigger(string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
+			=> RegisterTrigger(typeof(InputTrigger),name,bindings,minValue,maxValue);
+		public static T GetTrigger<T>() where T : SingletonInputTrigger
+			=> (T)Instance.triggers[SingletonInputTrigger.Info<T>.id];
+
+		internal static InputTrigger RegisterTrigger(Type type,string name,InputBinding[] bindings,float? minValue = null,float? maxValue = null)
+		{
+			var instance = Instance;
+
+			if(instance.triggersByName.TryGetValue(name,out var trigger)) {
+				trigger.Bindings = bindings;
+				return trigger;
+			}
+
+			int id = instance.triggers.Length;
+
+			trigger = (InputTrigger)Activator.CreateInstance(type,true); //new InputTrigger();
+
+			trigger.Init(id,name,bindings,minValue ?? InputTrigger.DefaultMinValue,maxValue ?? InputTrigger.DefaultMaxValue);
+
+			Array.Resize(ref instance.triggers,id+1);
+
+			instance.triggers[id] = trigger;
+			instance.triggersByName[name] = trigger;
+
+			InputTrigger.Count = instance.triggers.Length;
+
+			return trigger;
 		}
 	}
 }
