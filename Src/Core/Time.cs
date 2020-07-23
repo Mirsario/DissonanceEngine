4beb1748de55bc9f5f7dd4a118f65bce1cc41f19
Mirsario <me@mirsar.io>
2020-07-23 22:54:47 +0300

    Rewrote Time.cs from scratch, and fixed major issues with it.

--- Src/Core/Time.cs
+++ Src/Core/Time.cs
@@ -7,74 +7,65 @@ namespace Dissonance.Engine.Core
 {
 	public sealed class Time : EngineModule
 	{
-		//Target Framerate
-		internal static double targetUpdateFrequency = 60;
-		internal static double targetRenderFrequency = 0;
-		//Time
-		internal static float timeScale = 1f;
-		internal static float fixedTime; //Fixed time
-		internal static float fixedTimePrev;
-		internal static float fixedTimeReal;
-		internal static float fixedTimeRealPrev;
-		//internal static float fixedDeltaTime;
-		internal static uint fixedUpdateCount;
-		internal static float renderTime; //Render time
-		internal static float renderTimePrev;
-		internal static float renderTimeReal;
-		internal static float renderTimeRealPrev;
-		internal static float renderDeltaTime;
-		internal static uint renderUpdateCount;
+		private struct TimeData
+		{
+			//Time
+			public float game;
+			public float gameDelta;
+			public float global;
+			public float globalDelta;
+			//Framerate
+			public float ms;
+			public float msTemp;
+			public uint frameNum;
+			public uint framerate;
+			//Etc
+			public uint updateCount;
+		}
 
-		//Framerate Measuring
-		private static Stopwatch fixedStopwatch; //Fixed time
-		private static float fixedMs;
-		private static float fixedMsTemp;
-		private static uint fixedFrame;
-		private static uint fixedFPS;
-		private static Stopwatch renderStopwatch; //Render time
-		private static float renderMs;
-		private static float renderMsTemp;
-		private static uint renderFrame;
-		private static uint renderFPS;
+		internal static Time Instance => Game.Instance.GetModule<Time>(true);
 
-		//Time
-		public static float GameTime => Game.IsFixedUpdate ? fixedTime : renderTime;
-		public static float GlobalTime => Game.IsFixedUpdate ? fixedTimeReal : renderTimeReal;
-		public static float DeltaTime => Game.IsFixedUpdate ? FixedDeltaTime : RenderDeltaTime;
-
-		public static float FixedGameTime => fixedTime;
-		public static float RenderGameTime => renderTime;
-		public static float FixedGlobalTime => fixedTimeReal;
-		public static float RenderGlobalTime => renderTimeReal;
+		//Auto
+		public static float GameTime => Game.IsFixedUpdate ? FixedGameTime : RenderGameTime;
+		public static float GlobalTime => Game.IsFixedUpdate ? FixedGlobalTime : RenderGlobalTime;
+		public static float DeltaTime => Game.IsFixedUpdate ? FixedDeltaTime : RenderDeltaTime; //It might be a bit weird that this isn't 2 'GameDeltaTime' and 'GlobalDeltaTime' properties.
+		//Fixed Time
+		public static float FixedGameTime => Instance.fixedTime.game;
+		public static float FixedGlobalTime => Instance.fixedTime.global;
 		public static float FixedDeltaTime => 1f/(float)TargetUpdateFrequency;
-		public static float RenderDeltaTime => renderDeltaTime;
-		public static uint FixedUpdateCount => fixedUpdateCount;
-		public static uint RenderUpdateCount => renderUpdateCount;
-		//Framerate
-		public static uint FixedFramerate => fixedFPS;
-		public static float FixedMs => fixedMs;
-		public static uint RenderFramerate => renderFPS;
-		public static float RenderMs => renderMs;
+		public static uint FixedUpdateCount => Instance.fixedTime.updateCount;
+		//Render Time
+		public static float RenderGameTime => Instance.renderTime.game;
+		public static float RenderGlobalTime => Instance.renderTime.global;
+		public static float RenderDeltaTime => Instance.renderTime.gameDelta;
+		public static uint RenderUpdateCount => Instance.renderTime.updateCount;
+		//Fixed Framerate
+		public static uint FixedFramerate => Instance.fixedTime.framerate;
+		public static float FixedMs => Instance.fixedTime.ms;
+		//Render Framerate
+		public static uint RenderFramerate => Instance.renderTime.framerate;
+		public static float RenderMs => Instance.renderTime.ms;
 
 		//Time
 		public static float TimeScale {
-			get => timeScale;
+			get => Instance.timeScale;
 			set {
 				if(value<0f) {
 					throw new Exception("TimeScale cannot be negative.");
 				}
-				timeScale = value;
+
+				Instance.timeScale = value;
 			}
 		}
 		//Target Framerate
 		public static double TargetUpdateFrequency {
-			get => targetUpdateFrequency;
+			get => Instance.targetUpdateFrequency;
 			set {
 				if(value<=0) {
 					throw new ArgumentOutOfRangeException(nameof(value),"Value has to be more than zero.");
 				}
 
-				targetUpdateFrequency = value;
+				Instance.targetUpdateFrequency = value;
 
 				//TODO: Reimplement
 				/*if(Game.window!=null) {
@@ -83,13 +74,13 @@ namespace Dissonance.Engine.Core
 			}
 		}
 		public static double TargetRenderFrequency {
-			get => targetRenderFrequency;
+			get => Instance.targetRenderFrequency;
 			set {
 				if(value<0) {
 					throw new ArgumentOutOfRangeException(nameof(value),"Value has to be more than or equal to zero.");
 				}
 
-				targetRenderFrequency = value;
+				Instance.targetRenderFrequency = value;
 
 				//TODO: Reimplement
 				/*if(Game.window!=null) {
@@ -97,6 +88,19 @@ namespace Dissonance.Engine.Core
 				}*/
 			}
 		}
+
+		//Target Framerate
+		private double targetUpdateFrequency = 60;
+		private double targetRenderFrequency = 0;
+		//Time
+		private float timeScale = 1f;
+		private TimeData fixedTime;
+		private TimeData fixedTimePrev;
+		private TimeData renderTime;
+		private TimeData renderTimePrev;
+		//Framerate
+		private Stopwatch fixedStopwatch;
+		private Stopwatch renderStopwatch;
 
 		//Initialization
 		protected override void PreInit()
@@ -109,60 +113,48 @@ namespace Dissonance.Engine.Core
 			fixedStopwatch = new Stopwatch();
 			renderStopwatch = new Stopwatch();
 		}
+
 		//Fixed Update
 		[HookPosition(-100000)]
-		protected override void PreFixedUpdate()
-		{
-			fixedStopwatch.Restart();
+		protected override void PreFixedUpdate() => PreUpdate(ref fixedTime,ref fixedTimePrev,fixedStopwatch);
 
-			double newTime = FixedGlobalTime+1.0/TargetUpdateFrequency;
-
-			//Real time
-			fixedTimeRealPrev = fixedTimeReal;
-			fixedTimeReal = (float)newTime;
-
-			//Game time
-			fixedTimePrev = fixedTime;
-			fixedTime += FixedDeltaTime;
-
-			fixedUpdateCount++;
-		}
 		[HookPosition(100000)]
-		protected override void PostFixedUpdate() => MeasureFPS(ref fixedFPS,ref fixedFrame,fixedTime,fixedTimePrev,fixedStopwatch,ref fixedMs,ref fixedMsTemp);
+		protected override void PostFixedUpdate() => PostUpdate(ref fixedTime,ref fixedTimePrev,fixedStopwatch);
+
 		//Render Update
 		[HookPosition(-100000)]
-		protected override void PreRenderUpdate()
-		{
-			renderStopwatch.Restart();
+		protected override void PreRenderUpdate() => PreUpdate(ref renderTime,ref renderTimePrev,renderStopwatch);
 
-			double newTime = GLFW.GetTime();
-
-			//Real time
-			renderTimeRealPrev = renderTimeReal;
-			renderTimeReal = (float)newTime;
-
-			//Delta
-			renderDeltaTime = renderTimeReal-renderTimeRealPrev;
-
-			//Game time
-			renderTimePrev = renderTime;
-			renderTime += renderDeltaTime;
-
-			renderUpdateCount++;
-		}
 		[HookPosition(100000)]
-		protected override void PostRenderUpdate() => MeasureFPS(ref renderFPS,ref renderFrame,renderTime,renderTimePrev,renderStopwatch,ref renderMs,ref renderMsTemp);
+		protected override void PostRenderUpdate() => PostUpdate(ref renderTime,ref renderTimePrev,renderStopwatch);
 
-		private static void MeasureFPS(ref uint fps,ref uint frames,float time,float timePrev,Stopwatch stopwatch,ref float ms,ref float msTemp) //Move this somewhere
+		private void PreUpdate(ref TimeData currentTime,ref TimeData previousTime,Stopwatch stopwatch)
 		{
-			frames++;
-			msTemp += stopwatch.ElapsedMilliseconds;
+			stopwatch.Restart();
 
-			if(Mathf.FloorToInt(time)>Mathf.FloorToInt(timePrev)) {
-				fps = frames;
-				frames = 0;
-				ms = msTemp/Math.Max(1,fps);
-				msTemp = 0f;
+			currentTime.global = (float)GLFW.GetTime();
+			currentTime.globalDelta = currentTime.global-previousTime.global;
+			currentTime.gameDelta = currentTime.globalDelta*timeScale;
+			currentTime.game += currentTime.gameDelta;
+
+			currentTime.updateCount++;
+		}
+		private void PostUpdate(ref TimeData currentTime,ref TimeData previousTime,Stopwatch stopwatch)
+		{
+			MeasureFPS(ref currentTime,previousTime,stopwatch);
+
+			previousTime = currentTime;
+		}
+		private static void MeasureFPS(ref TimeData time,TimeData timePrev,Stopwatch stopwatch)
+		{
+			time.frameNum++;
+			time.msTemp += stopwatch.ElapsedMilliseconds;
+
+			if(Mathf.FloorToInt(time.global)>Mathf.FloorToInt(timePrev.global)) {
+				time.framerate = time.frameNum;
+				time.frameNum = 0;
+				time.ms = time.msTemp/Math.Max(1,time.framerate);
+				time.msTemp = 0f;
 			}
 		}
 	}
