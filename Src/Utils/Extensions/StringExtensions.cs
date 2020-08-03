ba4c2574d010c3afb419db6030518488bda6c622
Mirsario <me@mirsar.io>
2020-08-03 13:25:54 +0300

    Clean-ups for utils and extensions.

--- Src/Utils/Extensions/StringExtensions.cs
+++ Src/Utils/Extensions/StringExtensions.cs
@@ -1,14 +1,11 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Dissonance.Engine.Utils.Extensions
 {
 	public static class StringExtensions
 	{
-		public static int SizeInBytes(this string str) => Encoding.ASCII.GetByteCount(str);
-		public static bool IsEmptyOrNull(this string str) => string.IsNullOrEmpty(str);
-
-		public static string ReplaceCaseInsensitive(this string str,string oldValue,string newValue) => Regex.Replace(str,Regex.Escape(oldValue),newValue.Replace("$","$$"),RegexOptions.IgnoreCase);
+		public static string ReplaceCaseInsensitive(this string str,string oldValue,string newValue)
+			=> Regex.Replace(str,Regex.Escape(oldValue),newValue.Replace("$","$$"),RegexOptions.IgnoreCase);
 		public static string ReplaceCaseInsensitive(this string str,params (string oldValue, string newValue)[] replacements)
 		{
 			for(int i = 0;i<replacements.Length;i++) {
