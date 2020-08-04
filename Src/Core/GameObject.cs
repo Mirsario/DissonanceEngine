2d40df6a4ec897b499e8a987eabca9117f42da45
Mirsario <me@mirsar.io>
2020-08-04 15:50:38 +0300

    Added non-generic Type overloads for some Component methods.

--- Src/Core/GameObject.cs
+++ Src/Core/GameObject.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Reflection;
-using System.Collections.Generic;
-using Dissonance.Engine.Physics;
 using Dissonance.Engine.Core.ProgrammableEntities;
-using Dissonance.Engine.Core.Components;
+using Dissonance.Engine.Physics;
 using Dissonance.Engine.Structures;
 
 namespace Dissonance.Engine.Core
 {
-	public class GameObject : ProgrammableEntity, IDisposable
+	public partial class GameObject : ProgrammableEntity, IDisposable
 	{
 		private static GameObjectManager Manager => Game.Instance.GetModule<GameObjectManager>();
 
@@ -16,8 +13,6 @@ namespace Dissonance.Engine.Core
 		public string name;
 
 		internal bool initialized;
-		internal List<Component> components;
-		internal Dictionary<int,List<Component>> componentsByNameHash;
 		internal RigidbodyInternal rigidbodyInternal;
 		internal Transform transform;
 
@@ -32,13 +27,6 @@ namespace Dissonance.Engine.Core
 		public virtual void OnInit() { }
 		public virtual void OnDispose() { }
 
-		internal void PreInit()
-		{
-			Name = GetType().Name;
-			transform = new Transform(this);
-			components = new List<Component>();
-			componentsByNameHash = new Dictionary<int,List<Component>>();
-		}
 		public void Init()
 		{
 			if(initialized) {
@@ -58,117 +46,17 @@ namespace Dissonance.Engine.Core
 			ProgrammableEntityManager.UnsubscribeEntity(this);
 
 			OnDispose();
-
-			for(int i = 0;i<components.Count;i++) {
-				components[i].Dispose();
-			}
-
-			components.Clear();
-			components = null;
-
-			componentsByNameHash.Clear();
-			componentsByNameHash = null;
+			ComponentDispose();
 
 			Manager.gameObjects.Remove(this);
 		}
 
-		public T AddComponent<T>(Action<T> initializer) where T : Component => AddComponent(true,initializer);
-		public T AddComponent<T>(bool enable,Action<T> initializer) where T : Component
+		internal void PreInit()
 		{
-			T component = AddComponent<T>(false);
+			Name = GetType().Name;
+			transform = new Transform(this);
 
-			initializer(component);
-
-			if(enable) {
-				component.Enabled = true;
-			}
-
-			return component;
-		}
-		public T AddComponent<T>(bool enable = true) where T : Component
-		{
-			var componentType = typeof(T);
-			var parameters = ComponentManager.GetParameters(componentType);
-
-			if(parameters.allowOnlyOnePerObject) {
-				if(CountComponents<T>()>=1) {
-					throw new Exception("You can't add more than 1 component of type "+componentType.Name+" to a single gameobject");
-				}
-			}
-
-			Component newComponent;
-
-			try {
-				newComponent = (Component)Activator.CreateInstance(typeof(T));
-			}
-			catch(TargetInvocationException e) {
-				Debug.Log(e.InnerException);
-				return null;
-			}
-
-			int key = newComponent.NameHash;
-			if(!componentsByNameHash.TryGetValue(key,out var componentList)) {
-				componentsByNameHash[key] = componentList = new List<Component>();
-			}
-
-			componentList.Add(newComponent);
-			components.Add(newComponent);
-
-			newComponent.gameObject = this;
-			newComponent.PreInit();
-
-			if(enable) {
-				newComponent.Enabled = true;
-			}
-
-			return (T)newComponent;
-		}
-		public T GetComponent<T>(bool noSubclasses = false) where T : Component
-		{
-			var type = typeof(T);
-
-			for(int i = 0;i<components.Count;i++) {
-				var component = components[i];
-				var thisType = component.GetType();
-
-				if(thisType==type || !noSubclasses && thisType.IsSubclassOf(type)) {
-					return (T)component;
-				}
-			}
-
-			return null;
-		}
-		public IEnumerable<T> GetComponents<T>(bool noSubclasses = false) where T : Component
-		{
-			var list = new List<T>();
-			var type = typeof(T);
-
-			for(int i = 0;i<components.Count;i++) {
-				var component = components[i];
-				var thisType = component.GetType();
-
-				if(thisType==type || !noSubclasses && thisType.IsSubclassOf(type)) {
-					list.Add((T)component);
-				}
-			}
-
-			return list;
-		}
-		public int CountComponents<T>(bool noSubclasses = false) where T : Component
-		{
-			int count = 0;
-			var type = typeof(T);
-
-			for(int i = 0;i<components.Count;i++) {
-				var component = components[i];
-				var thisType = component.GetType();
-
-				if(thisType==type || !noSubclasses && thisType.IsSubclassOf(type)) {
-					count++;
-				}
-			}
-
-			return count;
+			ComponentPreInit();
 		}
 
 		public static T Instantiate<T>(string name = default,Vector3 position = default,Quaternion rotation = default,Vector3? scale = null,bool init = true) where T : GameObject
