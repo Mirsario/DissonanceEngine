e3d9323320a46e99400432856527b7755be8fcad
Mirsario <me@mirsar.io>
2020-08-04 15:50:09 +0300

    Added AssertionUtils class.

---
+++ Src/Utils/Internal/AssertionUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dissonance.Engine.Utils.Internal
+{
+	internal static class AssertionUtils
+	{
+		public static void TypeIsAssignableFrom(Type type,Type otherType)
+		{
+			if(!type.IsAssignableFrom(otherType)) {
+				throw new ArgumentException($"Type '{otherType.Name}' does not derive from '{type.Name}'.");
+			}
+		}
+	}
+}
