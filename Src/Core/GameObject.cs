68931876b6cf01fb25faa29700d0f5bfc8e3643b
Mirsario <me@mirsar.io>
2020-08-06 22:16:39 +0300

    Changed the way gameobjects are instantified.

--- Src/Core/GameObject.cs
+++ Src/Core/GameObject.cs
@@ -22,7 +22,13 @@ namespace Dissonance.Engine.Core
 			set => name = value ?? throw new Exception("GameObject's name cannot be set to null");
 		}
 
-		protected GameObject() : base() { }
+		protected GameObject() : base()
+		{
+			Name = GetType().Name;
+			transform = new Transform(this);
+
+			ComponentPreInit();
+		}
 
 		public virtual void OnInit() { }
 		public virtual void OnDispose() { }
@@ -51,17 +57,9 @@ namespace Dissonance.Engine.Core
 			Manager.gameObjects.Remove(this);
 		}
 
-		internal void PreInit()
-		{
-			Name = GetType().Name;
-			transform = new Transform(this);
-
-			ComponentPreInit();
-		}
-
-		public static T Instantiate<T>(string name = default,Vector3 position = default,Quaternion rotation = default,Vector3? scale = null,bool init = true) where T : GameObject
-			=> Manager.Instantiate<T>(name,position,rotation,scale,init);
-		public static GameObject Instantiate(Type type,string name = default,Vector3? position = default,Quaternion? rotation = default,Vector3? scale = null,bool init = true)
-			=> Manager.Instantiate(type,name,position,rotation,scale,init);
+		public static T Instantiate<T>(Action<T> preinitializer = null) where T : GameObject
+			=> Manager.Instantiate(preinitializer);
+		public static GameObject Instantiate(Type type,Action<GameObject> preinitializer = null)
+			=> Manager.Instantiate(type,preinitializer);
 	}
 }
