e3bfae4c5b52b98fa4277d518876ef65355fc675
Mirsario <me@mirsar.io>
2021-04-07 01:06:44 +0300

    Added RequireComponentAttribute

---
+++ Src/Core/Components/Attributes/RequireComponentAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Dissonance.Engine.Utilities;
+
+namespace Dissonance.Engine
+{
+	public class RequireComponentAttribute : ComponentAttribute
+	{
+		public readonly Type RequiredType;
+
+		public RequireComponentAttribute(Type componentType)
+		{
+			AssertionUtils.TypeIsAssignableFrom(typeof(Component), componentType);
+
+			RequiredType = componentType;
+		}
+
+		public override void PreAddComponent(GameObject gameObject, Type type)
+		{
+			if(!gameObject.Components.Any(c => c.GetType().IsAssignableFrom(RequiredType))) {
+				throw new InvalidOperationException($"Expected '{RequiredType.Name}' component (required by '{type.Name}') was not found on '{gameObject.Name}' GameObject.");
+			}
+		}
+	}
+}
