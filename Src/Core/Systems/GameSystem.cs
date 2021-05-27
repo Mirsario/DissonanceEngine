5bdc2070d5eb8c751cb518e190c7888e96b06707
Mirsario <me@mirsar.io>
2021-05-27 23:24:57 +0300

    Marked World methods internal again and placed public versions on GameSystem

--- Src/Core/Systems/GameSystem.cs
+++ Src/Core/Systems/GameSystem.cs
@@ -1,4 +1,6 @@
-﻿namespace Dissonance.Engine
+﻿using System;
+
+namespace Dissonance.Engine
 {
 	public abstract class GameSystem
 	{
@@ -7,5 +9,17 @@
 		public virtual void Initialize() { }
 		public virtual void FixedUpdate() { }
 		public virtual void RenderUpdate() { }
+
+		public bool Has<T>() where T : struct, IComponent
+			=> World.Has<T>();
+
+		public ref T Get<T>() where T : struct, IComponent
+			=> ref World.Get<T>();
+
+		public void Set<T>(T value) where T : struct, IComponent
+			=> World.Set(value);
+
+		public ReadOnlySpan<Entity> ReadEntities()
+			=> World.ReadEntities();
 	}
 }
