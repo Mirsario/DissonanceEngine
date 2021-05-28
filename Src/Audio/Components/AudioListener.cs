a64e7f4e3647d8d07a2c64a5251edb59bbf625da
Mirsario <me@mirsar.io>
2021-05-28 19:13:31 +0300

    Fixed audio listeners and 3d audio.

--- Src/Audio/Components/AudioListener.cs
+++ Src/Audio/Components/AudioListener.cs
@@ -3,24 +3,7 @@ namespace Dissonance.Engine.Audio
 	[AllowOnlyOneInWorld]
 	public struct AudioListener : IComponent
 	{
-		/*protected internal override void FixedUpdate()
-		{
-			Vector3 pos = Transform.Position;
-
-			if(pos.HasNaNs) {
-				throw new Exception($"NaNs values detected in {GameObject.GetType().Name}'s Transform.");
-			}
-
-			AL.Listener3(ListenerFloat3.Position, pos.x, pos.y, pos.z);
-
-			Vector3 lookAt = -Transform.Forward;
-			Vector3 up = Transform.Up;
-
-			AL.Listener(ListenerFloatArray.Orientation, new[] {
-				lookAt.x,   lookAt.y,   lookAt.z,
-				up.x,       up.y,       up.z
-			});
-		}*/
+		public static readonly AudioListener Default = new AudioListener();
 	}
 }
 
