98a6c887d13c874e510ff3bc1fdf378abdddc5c3
Mirsario <me@mirsar.io>
2021-07-27 14:48:33 +0300

    Publicized many World methods.

--- Src/Core/World.cs
+++ Src/Core/World.cs
@@ -26,7 +26,7 @@ namespace Dissonance.Engine
 		public EntitySet GetEntitySet(EntitySetType type, Expression<Predicate<Entity>> expression)
 			=> EntityManager.GetEntitySet(Id, type, expression);
 
-		internal ReadOnlySpan<Entity> ReadEntities(bool? active = true)
+		public ReadOnlySpan<Entity> ReadEntities(bool? active = true)
 			=> EntityManager.ReadEntities(Id, active);
 
 		// Systems
@@ -36,21 +36,21 @@ namespace Dissonance.Engine
 
 		// Components
 
-		internal bool Has<T>() where T : struct
+		public bool Has<T>() where T : struct
 			=> ComponentManager.HasComponent<T>(Id);
 
-		internal ref T Get<T>() where T : struct
+		public ref T Get<T>() where T : struct
 			=> ref ComponentManager.GetComponent<T>(Id);
 
-		internal void Set<T>(T value) where T : struct
+		public void Set<T>(T value) where T : struct
 			=> ComponentManager.SetComponent(Id, value);
 
 		// Messages
 
-		internal ReadOnlySpan<T> ReadMessages<T>() where T : struct
+		public ReadOnlySpan<T> ReadMessages<T>() where T : struct
 			=> MessageManager.ReadMessages<T>(Id);
 
-		internal void SendMessage<T>(in T message) where T : struct
+		public void SendMessage<T>(in T message) where T : struct
 			=> MessageManager.SendMessage(Id, message);
 	}
 }
