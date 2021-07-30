aef15a3ae1a1b9b429c5010d3e5c6a8d43bc98f7
Mirsario <me@mirsar.io>
2021-07-30 14:35:45 +0300

    Added basic UV maps to icospheres, partially cleaned up PrimitiveMeshes.cs

--- Src/Graphics/Meshes/PrimitiveMeshes.cs
+++ Src/Graphics/Meshes/PrimitiveMeshes.cs
@@ -1,4 +1,6 @@
-﻿namespace Dissonance.Engine.Graphics
+﻿using System;
+
+namespace Dissonance.Engine.Graphics
 {
 	public static class PrimitiveMeshes
 	{
@@ -19,8 +21,8 @@
 			var newMesh = new Mesh {
 				//Vertices
 				Vertices = new[] {
-					new Vector3(-half,-half,0f),    new Vector3( half,-half,0f),
-					new Vector3(-half, half,0f),    new Vector3( half, half,0f),
+					new Vector3(-half,-half, 0f),    new Vector3( half,-half, 0f),
+					new Vector3(-half, half, 0f),    new Vector3( half, half, 0f),
 				},
 
 				//UVs
@@ -31,8 +33,8 @@
 
 				//Triangles
 				Indices = new uint[] {
-					2,1,0,
-					2,3,1,
+					2, 1, 0,
+					2, 3, 1,
 				}
 			};
 
@@ -50,6 +52,7 @@
 
 			return newMesh;
 		}
+		
 		public static Mesh GeneratePlane(Vector2Int resolution, Vector2 size, bool centered, bool addUVs = true, bool addNormals = true, bool addTangents = true, bool apply = true, Vector2? uvSize = null)
 		{
 			Vector2 offset = centered ? -size * 0.5f : Vector2.Zero;
@@ -114,6 +117,7 @@
 
 			return newMesh;
 		}
+		
 		public static Mesh GenerateCube(float cubeSize = 1f, bool inverted = false, bool addUVs = true, bool addNormals = true, bool addTangents = true, bool apply = true)
 		{
 			Vector3 size = Vector3.One * cubeSize;
@@ -122,12 +126,12 @@
 			var newMesh = new Mesh {
 				//Vertices
 				Vertices = new[] {
-					offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(0f    ,0f    ,size.z),   offset+new Vector3(size.x,0f    ,size.z),
-					offset+new Vector3(size.x,size.y,0f    ),   offset+new Vector3(0f    ,size.y,0f    ),   offset+new Vector3(size.x,0f    ,0f    ),   offset+new Vector3(0f    ,0f    ,0f    ),
-					offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(0f    ,size.y,0f    ),   offset+new Vector3(size.x,size.y,0f    ),
-					offset+new Vector3(0f    ,0f    ,size.z),   offset+new Vector3(size.x,0f    ,size.z),   offset+new Vector3(0f    ,0f    ,0f    ),   offset+new Vector3(size.x,0f    ,0f    ),
-					offset+new Vector3(0f    ,size.y,0f    ),   offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(0f    ,0f    ,0f    ),   offset+new Vector3(0f    ,0f    ,size.z),
-					offset+new Vector3(size.x,size.y,0f    ),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(size.x,0f    ,0f    ),   offset+new Vector3(size.x,0f    ,size.z)
+					offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(0f    , 0f    ,size.z),   offset+new Vector3(size.x, 0f    ,size.z),
+					offset+new Vector3(size.x,size.y, 0f    ),   offset+new Vector3(0f    ,size.y, 0f    ),   offset+new Vector3(size.x, 0f    , 0f    ),   offset+new Vector3(0f    , 0f    , 0f    ),
+					offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(0f    ,size.y, 0f    ),   offset+new Vector3(size.x,size.y, 0f    ),
+					offset+new Vector3(0f    , 0f    ,size.z),   offset+new Vector3(size.x, 0f    ,size.z),   offset+new Vector3(0f    , 0f    , 0f    ),   offset+new Vector3(size.x, 0f    , 0f    ),
+					offset+new Vector3(0f    ,size.y, 0f    ),   offset+new Vector3(0f    ,size.y,size.z),   offset+new Vector3(0f    , 0f    , 0f    ),   offset+new Vector3(0f    , 0f    ,size.z),
+					offset+new Vector3(size.x,size.y, 0f    ),   offset+new Vector3(size.x,size.y,size.z),   offset+new Vector3(size.x, 0f    , 0f    ),   offset+new Vector3(size.x, 0f    ,size.z)
 				},
 
 				//Normals
@@ -142,41 +146,41 @@
 
 				//UVs
 				Uv0 = addUVs ? new[] {
-					new Vector2(0f,0f),new Vector2(1f,0f),new Vector2(0f,1f),new Vector2(1f,1f),
-					new Vector2(0f,0f),new Vector2(1f,0f),new Vector2(0f,1f),new Vector2(1f,1f),
-					new Vector2(1f,0f),new Vector2(0f,0f),new Vector2(1f,1f),new Vector2(0f,1f),
-					new Vector2(0f,0f),new Vector2(1f,0f),new Vector2(0f,1f),new Vector2(1f,1f),
-					new Vector2(0f,0f),new Vector2(1f,0f),new Vector2(0f,1f),new Vector2(1f,1f),
-					new Vector2(1f,0f),new Vector2(0f,0f),new Vector2(1f,1f),new Vector2(0f,1f)
+					new Vector2(0f, 0f),new Vector2(1f, 0f),new Vector2(0f, 1f),new Vector2(1f, 1f),
+					new Vector2(0f, 0f),new Vector2(1f, 0f),new Vector2(0f, 1f),new Vector2(1f, 1f),
+					new Vector2(1f, 0f),new Vector2(0f, 0f),new Vector2(1f, 1f),new Vector2(0f, 1f),
+					new Vector2(0f, 0f),new Vector2(1f, 0f),new Vector2(0f, 1f),new Vector2(1f, 1f),
+					new Vector2(0f, 0f),new Vector2(1f, 0f),new Vector2(0f, 1f),new Vector2(1f, 1f),
+					new Vector2(1f, 0f),new Vector2(0f, 0f),new Vector2(1f, 1f),new Vector2(0f, 1f)
 				} : null,
 
 				//Triangles
 				Indices = inverted ? new uint[] {
-					1,2,0, //Inverted
-					3,2,1,
-					5,6,4,
-					7,6,5,
-					9,8,10,
-					11,9,10,
-					13,14,12,
-					15,14,13,
-					17,18,16,
-					19,18,17,
-					21,20,22,
-					23,21,22
+					1, 2, 0, //Inverted
+					3, 2, 1,
+					5, 6, 4,
+					7, 6, 5,
+					9, 8, 10,
+					11, 9, 10,
+					13, 14, 12,
+					15, 14, 13,
+					17, 18, 16,
+					19, 18, 17,
+					21, 20, 22,
+					23, 21, 22
 				} : new uint[] {
-					2,1,0, //Normal
-					2,3,1,
-					6,5,4,
-					6,7,5,
-					8,9,10,
-					9,11,10,
-					14,13,12,
-					14,15,13,
-					18,17,16,
-					18,19,17,
-					20,21,22,
-					21,23,22
+					2, 1, 0, //Normal
+					2, 3, 1,
+					6, 5, 4,
+					6, 7, 5,
+					8, 9, 10,
+					9, 11, 10,
+					14, 13, 12,
+					14, 15, 13,
+					18, 17, 16,
+					18, 19, 17,
+					20, 21, 22,
+					21, 23, 22
 				}
 			};
 
@@ -194,6 +198,7 @@
 
 			return newMesh;
 		}
+		
 		public static Mesh GenerateSphere(int xRes = 16, int yRes = 16, float radius = 1f, bool inverted = false, bool addUVs = true, bool addNormals = true, bool addTangents = true, bool apply = true)
 		{
 			//TODO: There's plenty of unneeded vertex repeating, which is only needed on UV seams.
@@ -266,49 +271,61 @@
 
 			return newMesh;
 		}
-		public static Mesh GenerateIcoSphere(float size = 1f, bool inverted = false, bool addNormals = true, bool addTangents = true, bool apply = true)
+		
+		public static Mesh GenerateIcoSphere(float size = 1f, bool inverted = false, bool addUVs = true, bool addNormals = true, bool addTangents = true, bool apply = true)
 		{
-			//TODO: Reimplement with an algorithm that accepts a resolution integer
-			//TODO: Add UVs and make tangents work
-			const float N = 0f;
-			//float X = 0.525731112119133606f;
-			//float Z = 0.850650808352039932f;
-			float X = 0.61803398874989484830630790701031f * size;
-			float Z = 1f * size;
+			float a = (1f + Mathf.Sqrt(5)) * size * 0.5f;
+			float b = size;
 
 			var newMesh = new Mesh {
-				//Vertices
-				Vertices = new[] {
-					new Vector3(-X,N,Z),new Vector3( X, N, Z),new Vector3(-X, N,-Z),new Vector3( X, N,-Z),
-					new Vector3( N,Z,X),new Vector3( N, Z,-X),new Vector3( N,-Z, X),new Vector3( N,-Z,-X),
-					new Vector3( Z,X,N),new Vector3(-Z, X, N),new Vector3( Z,-X, N),new Vector3(-Z,-X, N)
+				Vertices = new Vector3[] {
+					new(-b, 0, a), new( b, 0, a), new(-b, 0,-a), new( b, 0,-a),
+					new( 0, a, b), new( 0, a,-b), new( 0,-a, b), new( 0,-a,-b),
+					new( a, b, 0), new(-a, b, 0), new( a,-b, 0), new(-a,-b, 0)
 				},
-
-				//Triangles
-				Indices = inverted ? new uint[] {
-					//Inverted
-					4,0,1,   9,0,4,  5,9,4,  5,4,8,
-					8,4,1,  10,8,1, 3,8,10, 3,5,8,
-					2,5,3,  7,2,3,  10,7,3, 6,7,10,
-					11,7,6, 0,11,6, 1,0,6,  1,6,10,
-					0,9,11, 11,9,2, 2,9,5,  2,7,11
+				Indices = !inverted ? new uint[] {
+					4, 0, 1, 9, 0, 4, 5, 9, 4, 5, 4, 8,
+					8, 4, 1, 10, 8, 1, 3, 8, 10, 3, 5, 8,
+					2, 5, 3, 7, 2, 3, 10, 7, 3, 6, 7, 10,
+					11, 7, 6, 0, 11, 6, 1, 0, 6, 1, 6, 10,
+					0, 9, 11, 11, 9, 2, 2, 9, 5, 2, 7, 11
 				} : new uint[] {
-					//Normal
-					0,4,1,   0,9,4,  9,5,4,  4,5,8,
-					4,8,1,  8,10,1, 8,3,10, 5,3,8,
-					5,2,3,  2,7,3,  7,10,3, 7,6,10,
-					7,11,6, 11,0,6, 0,1,6,  6,1,10,
-					9,0,11, 9,11,2, 9,2,5,  7,2,11
-				}
+					0, 4, 1, 0, 9, 4, 9, 5, 4, 4, 5, 8,
+					4, 8, 1, 8, 10, 1, 8, 3, 10, 5, 3, 8,
+					5, 2, 3, 2, 7, 3, 7, 10, 3, 7, 6, 10,
+					7, 11, 6, 11, 0, 6, 0, 1, 6, 6, 1, 10,
+					9, 0, 11, 9, 11, 2, 9, 2, 5, 7, 2, 11
+				},
 			};
+
+			//Array.Resize(ref newMesh.Vertices, numVertices + numDuplicates);
+
+			var vertices = newMesh.Vertices;
+
+			// Normalize vertices.
+			for(int i = 0; i < vertices.Length; i++) {
+				vertices[i].Normalize();
+			}
+
+			// Generate UV maps.
+			var uv = newMesh.Uv0 = new Vector2[vertices.Length];
+			
+			for(int i = 0; i < vertices.Length; i++) {
+				var vertex = vertices[i];
+			
+				uv[i] = new Vector2(
+					(MathF.Atan2(vertex.z, vertex.x) / (2f * MathF.PI)) + 0.5f,
+					(MathF.Asin(vertex.y) / MathF.PI) - 0.5f
+				);
+			}
 
 			if(addNormals) {
 				newMesh.NormalBuffer.Recalculate();
 			}
 
-			/*if(addTangents) {
-				newMesh.RecalculateTangents();
-			}*/
+			if(addTangents) {
+				newMesh.TangentBuffer.Recalculate();
+			}
 
 			if(apply) {
 				newMesh.Apply();
