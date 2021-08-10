2c47d884cde3023b6999d3e5c5df1bc5cd084ecc
Mirsario <me@mirsar.io>
2021-08-10 22:39:42 +0300

    Added new special messages for components being added and removed.

---
+++ Src/Core/Messages/ComponentAddedMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Dissonance.Engine
+{
+	public readonly struct ComponentAddedMessage<T> where T : struct
+	{
+		public readonly Entity Entity;
+		public readonly T Value;
+
+		internal ComponentAddedMessage(Entity entity, T value)
+		{
+			Entity = entity;
+			Value = value;
+		}
+	}
+}
