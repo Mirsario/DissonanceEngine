e21ed39018b0dfa010a3a94a1025ece1d8538c01
Mirsario <me@mirsar.io>
2021-08-16 22:04:48 +0300

    Reimplemented CapsuleCollider.

--- Src/Physics/Collision/Colliders/CapsuleCollider.cs
+++ Src/Physics/Collision/Colliders/CapsuleCollider.cs
@@ -2,8 +2,41 @@ using BulletSharp;
 
 namespace Dissonance.Engine.Physics
 {
-	public class CapsuleCollider
+	public struct CapsuleCollider
 	{
+		public static readonly CapsuleCollider Default = new(1f, 2f);
 
+		internal CapsuleShape capsuleShape;
+		internal bool needsUpdate;
+
+		private float radius;
+		private float height;
+
+		public float Radius {
+			get => radius;
+			set {
+				if(value != radius) {
+					radius = value;
+					needsUpdate = true;
+				}
+			}
+		}
+
+		public float Height {
+			get => height;
+			set {
+				if(value != height) {
+					height = value;
+					needsUpdate = true;
+				}
+			}
+		}
+
+		public CapsuleCollider(float radius, float height) : this()
+		{
+			this.radius = radius;
+			this.height = height;
+			needsUpdate = true;
+		}
 	}
 }
