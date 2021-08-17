b2ef589c080cd99a5fb9081b38e5f1584d84ef6c
Mirsario <me@mirsar.io>
2021-08-17 18:26:55 +0300

    Merged Rigidbody and RigidbodyInternal. The component is now required for static colliision.

---
+++ Src/Physics/Collision/CollisionShapesInfoSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using BulletSharp;
+
+namespace Dissonance.Engine.Physics
+{
+	[Reads(typeof(CollisionShapesInfo))]
+	[Writes(typeof(CollisionShapesInfo))]
+	[Receives(typeof(AddCollisionShapeMessage), typeof(RemoveCollisionShapeMessage))]
+	public sealed class CollisionShapesInfoSystem : GameSystem
+	{
+		protected internal override void FixedUpdate()
+		{
+			// Remove collision shapes
+			foreach(var message in ReadMessages<RemoveCollisionShapeMessage>()) {
+				if(message.Entity.Has<CollisionShapesInfo>()) {
+					var shapes = message.Entity.Get<CollisionShapesInfo>().collisionShapes;
+
+					shapes.Remove(message.CollisionShape);
+
+					if(shapes.Count == 0) {
+						message.Entity.Remove<CollisionShapesInfo>();
+					}
+				}
+			}
+
+			// Register collision shapes
+			foreach(var message in ReadMessages<AddCollisionShapeMessage>()) {
+				List<CollisionShape> shapes;
+
+				if(!message.Entity.Has<CollisionShapesInfo>()) {
+					shapes = new();
+
+					message.Entity.Set(new CollisionShapesInfo {
+						collisionShapes = shapes
+					});
+				} else {
+					shapes = message.Entity.Get<CollisionShapesInfo>().collisionShapes;
+				}
+
+				shapes.Add(message.CollisionShape);
+			}
+		}
+	}
+}
