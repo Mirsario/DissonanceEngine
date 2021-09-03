64674f70bdd12b0b915870518c579695ecbb86e1
Mirsario <me@mirsar.io>
2021-09-03 19:36:13 +0300

    Split Geometry and Lighting render data into its files.

---
+++ Src/Graphics/RenderPasses/Default/LightingPassData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Dissonance.Engine.Graphics
+{
+	public struct LightingPassData : IRenderComponent
+	{
+		public struct LightData
+		{
+			public Light.LightType Type;
+			public Matrix4x4 Matrix;
+			public float Intensity;
+			public float? Range;
+			public Vector3 Color;
+			public Vector3? Position;
+			public Vector3? Direction;
+		}
+
+		public List<LightData> Lights { get; private set; }
+
+		public void Reset()
+		{
+			if (Lights == null) {
+				Lights = new List<LightData>();
+			} else {
+				Lights.Clear();
+			}
+		}
+	}
+}
