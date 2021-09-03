a92a61479e06958524ee2478a91266deb5b7c160
Mirsario <me@mirsar.io>
2021-09-03 19:36:13 +0300

    Split Geometry and Lighting render data into its files.

--- Src/Graphics/RenderPasses/Default/GeometryPass.cs
+++ Src/Graphics/RenderPasses/Default/GeometryPass.cs
@@ -1,39 +1,10 @@
-﻿using System.Collections.Generic;
-using Dissonance.Framework.Graphics;
+﻿using Dissonance.Framework.Graphics;
 
 namespace Dissonance.Engine.Graphics
 {
-	public struct GeometryPassData : IRenderComponent
-	{
-		public readonly struct RenderEntry
-		{
-			public readonly Transform transform;
-			public readonly Mesh mesh;
-			public readonly Material material;
-
-			public RenderEntry(Transform transform, Mesh mesh, Material material)
-			{
-				this.transform = transform;
-				this.mesh = mesh;
-				this.material = material;
-			}
-		}
-
-		public List<RenderEntry> RenderEntries { get; private set; }
-
-		public void Reset()
-		{
-			if (RenderEntries != null) {
-				RenderEntries.Clear();
-			} else {
-				RenderEntries = new();
-			}
-		}
-	}
-
 	public class GeometryPass : RenderPass
 	{
-		public ulong? layerMask;
+		//public ulong? layerMask;
 
 		public override void Render()
 		{
---
+++ Src/Graphics/RenderPasses/Default/GeometryPassData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Dissonance.Engine.Graphics
+{
+	public struct GeometryPassData : IRenderComponent
+	{
+		public readonly struct RenderEntry
+		{
+			public readonly Transform transform;
+			public readonly Mesh mesh;
+			public readonly Material material;
+
+			public RenderEntry(Transform transform, Mesh mesh, Material material)
+			{
+				this.transform = transform;
+				this.mesh = mesh;
+				this.material = material;
+			}
+		}
+
+		public List<RenderEntry> RenderEntries { get; private set; }
+
+		public void Reset()
+		{
+			if (RenderEntries != null) {
+				RenderEntries.Clear();
+			} else {
+				RenderEntries = new();
+			}
+		}
+	}
+}
