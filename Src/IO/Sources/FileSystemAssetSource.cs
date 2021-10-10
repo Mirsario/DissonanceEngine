b1dae052edd239d0e0d5786760c4a1519a52c14c
Mirsario <me@mirsar.io>
2021-10-11 01:27:58 +0300

    Fixed many things about FileSystemAssetSource.

--- Src/IO/Sources/FileSystemAssetSource.cs
+++ Src/IO/Sources/FileSystemAssetSource.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 
 namespace Dissonance.Engine.IO
@@ -12,19 +11,23 @@ namespace Dissonance.Engine.IO
 		public FileSystemAssetSource(string basePath)
 		{
 			this.basePath = basePath;
+
+			RecalculatePathsCache();
 		}
 
 		public override IEnumerable<string> EnumerateAssets() => assetPathToFullPath.Keys;
 
 		public override bool HasAsset(string path) => assetPathToFullPath.ContainsKey(path);
 
-		public override Stream OpenStream(string path) => File.OpenRead(path);
+		public override Stream OpenStream(string path) => File.OpenRead(assetPathToFullPath[path]);
 
 		public void RecalculatePathsCache()
 		{
-			foreach (var file in new DirectoryInfo(basePath).EnumerateFiles(null, SearchOption.AllDirectories)) {
+			foreach (var file in new DirectoryInfo(basePath).EnumerateFiles("*", SearchOption.AllDirectories)) {
 				string filePath = file.FullName;
 				string relativeFilePath = Path.GetRelativePath(basePath, filePath);
+
+				relativeFilePath = relativeFilePath.Replace('\\', '/');
 
 				assetPathToFullPath[relativeFilePath] = filePath;
 			}
