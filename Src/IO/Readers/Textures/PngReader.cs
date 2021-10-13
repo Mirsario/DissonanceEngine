a9c0b1c8668767150aad1f5a9002cc88087706e6
Mirsario <me@mirsar.io>
2021-10-13 21:19:27 +0300

    Fixed random texture corruption.

--- Src/IO/Readers/Textures/PngReader.cs
+++ Src/IO/Readers/Textures/PngReader.cs
@@ -1,7 +1,5 @@
 using System;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Dissonance.Engine.Graphics;
 using SixLabors.ImageSharp;
@@ -16,28 +14,28 @@ namespace Dissonance.Engine.IO
 
 		public async ValueTask<Texture> ReadFromStream(Stream stream, string assetPath, MainThreadCreationContext switchToMainThread)
 		{
-			var (width, height, data) = LoadImageData(stream);
+			var (width, height, pixels) = LoadImageData<Rgba32>(stream);
 
 			await switchToMainThread; // Switches context to the main thread for texture uploading.
 
 			var texture = new Texture(width, height);
 
-			texture.SetPixels(data);
+			texture.SetPixels(pixels);
 
 			return texture;
 		}
 
 		// Has to be split because async methods can't work with ref structures like Span<T>.
-		private static unsafe (int width, int height, IntPtr data) LoadImageData(Stream stream)
+		private static unsafe (int width, int height, TPixel[] pixels) LoadImageData<TPixel>(Stream stream) where TPixel : unmanaged, IPixel<TPixel>
 		{
 			var decoder = new PngDecoder();
-			var image = Image.Load<Rgba32>(stream, decoder);
+			var image = Image.Load<TPixel>(stream, decoder);
 
 			if (!image.TryGetSinglePixelSpan(out var span)) {
 				throw new InvalidOperationException($"Internal error in {nameof(PngReader)}.");
 			}
 
-			var data = (IntPtr)Unsafe.AsPointer(ref span.GetPinnableReference());
+			var data = span.ToArray();
 
 			return (image.Width, image.Height, data);
 		}
