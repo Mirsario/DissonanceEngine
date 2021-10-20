8c96a217b5cff0f5dcd9d1e691df55c833964d89
Mirsario <me@mirsar.io>
2021-10-20 19:00:24 +0300

    EntitySpawningSystem & SpawnEntityMessage.

---
+++ Src/Core/ECS/Entities/EntitySpawningSystem.cs
@@ -0,0 +1,21 @@
+﻿using Dissonance.Engine.IO;
+
+namespace Dissonance.Engine
+{
+	[Receives<SpawnEntityMessage>]
+	public sealed class EntitySpawningSystem : GameSystem
+	{
+		protected internal override void FixedUpdate() => Update();
+		protected internal override void RenderUpdate() => Update();
+
+		private void Update()
+		{
+			foreach (var message in ReadMessages<SpawnEntityMessage>()) {
+				var prefab = Assets.Get<PackedEntity>(message.PrefabAssetPath, AssetRequestMode.ImmediateLoad).Value;
+				var entity = prefab.Unpack(World);
+
+				message.Action?.Invoke(entity);
+			}
+		}
+	}
+}
---
+++ Src/Core/ECS/Entities/SpawnEntityMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dissonance.Engine
+{
+	public readonly struct SpawnEntityMessage
+	{
+		public readonly string PrefabAssetPath;
+		public readonly Action<Entity> Action;
+
+		public SpawnEntityMessage(string prefabAssetPath, Action<Entity> setupAction = null)
+		{
+			PrefabAssetPath = prefabAssetPath;
+			Action = setupAction;
+		}
+	}
+}
