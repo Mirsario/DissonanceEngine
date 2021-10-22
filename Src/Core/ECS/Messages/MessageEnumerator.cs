a71f114101aba0ff0b1f349b247606a91f585a8e
Mirsario <me@mirsar.io>
2021-10-22 23:39:58 +0300

    Fixed issues with sending messages while reading ones of the same type.

---
+++ Src/Core/ECS/Messages/MessageEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Dissonance.Engine
+{
+	public ref struct MessageEnumerator<T>
+	{
+		private readonly List<T> list;
+
+		private int i;
+		private int count;
+		private T current;
+
+		public T Current => current;
+
+		public MessageEnumerator(List<T> list)
+		{
+			this.list = list;
+
+			i = -1;
+			count = list.Count;
+			current = default;
+		}
+
+		public bool MoveNext()
+		{
+			if (++i < count || i < (count = list?.Count ?? 0)) {
+				current = list[i];
+
+				return true;
+			}
+
+			current = default;
+
+			return false;
+		}
+
+		public MessageEnumerator<T> GetEnumerator() => this;
+	}
+}
