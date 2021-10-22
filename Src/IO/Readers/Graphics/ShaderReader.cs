ec90baacccfb4c6a6ac5b89d5163277802d2f658
Mirsario <me@mirsar.io>
2021-10-22 23:56:02 +0300

    Fixed shader programs not being able to use relative paths.

--- Src/IO/Readers/Graphics/ShaderReader.cs
+++ Src/IO/Readers/Graphics/ShaderReader.cs
@@ -15,8 +15,9 @@ namespace Dissonance.Engine.IO
 
 		public async ValueTask<Asset<Shader>[]> ReadFromStream(Stream stream, string assetPath, MainThreadCreationContext switchToMainThread)
 		{
+			string directory = Assets.FilterPath(Path.GetDirectoryName(assetPath));
 			using var reader = new StreamReader(stream);
-			
+
 			string jsonText = reader.ReadToEnd();
 			var shaders = new List<Asset<Shader>>();
 
@@ -28,9 +29,9 @@ namespace Dissonance.Engine.IO
 				string name = pair.Key;
 				var jsonShader = pair.Value;
 
-				string vertexCode = Assets.Get<string>(jsonShader.VertexShader, AssetRequestMode.ImmediateLoad).Value;
-				string fragmentCode = Assets.Get<string>(jsonShader.FragmentShader, AssetRequestMode.ImmediateLoad).Value;
-				string geometryCode = !string.IsNullOrWhiteSpace(jsonShader.GeometryShader) ? Assets.Get<string>(jsonShader.GeometryShader, AssetRequestMode.ImmediateLoad).Value : null;
+				string vertexCode = Assets.Get<string>(jsonShader.VertexShader, directory, AssetRequestMode.ImmediateLoad).Value;
+				string fragmentCode = Assets.Get<string>(jsonShader.FragmentShader, directory, AssetRequestMode.ImmediateLoad).Value;
+				string geometryCode = !string.IsNullOrWhiteSpace(jsonShader.GeometryShader) ? Assets.Get<string>(jsonShader.GeometryShader, directory, AssetRequestMode.ImmediateLoad).Value : null;
 
 				var shader = Shader.FromCode(name, vertexCode, fragmentCode, geometryCode, jsonShader.ShaderDefines);
 
