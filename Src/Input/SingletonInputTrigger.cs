0bfb6d47d3cbf3dda6f28d08f9ce15c4aea5d3a3
Mirsario <me@mirsar.io>
2022-03-20 19:38:14 +0300

    Rewrote SingletonInputTrigger inits to not use AssemblyCache.

--- Src/Input/SingletonInputTrigger.cs
+++ Src/Input/SingletonInputTrigger.cs
@@ -6,11 +6,6 @@ namespace Dissonance.Engine.Input
 {
 	public abstract class SingletonInputTrigger : InputTrigger
 	{
-		internal static class Info<T> where T : SingletonInputTrigger
-		{
-			public static int id;
-		}
-
 		protected SingletonInputTrigger() : base() { }
 
 		protected abstract void Init(out string name, out InputBinding[] bindings, out float minValue, out float maxValue);
@@ -20,21 +15,6 @@ namespace Dissonance.Engine.Input
 			Init(out name, out bindings, out float newMinValue, out float newMaxValue);
 
 			base.Init(id, name, bindings, newMinValue, newMaxValue);
-		}
-
-		internal static void StaticInit()
-		{
-			foreach (var type in AssemblyCache.AllTypes) {
-				if (type.IsAbstract || !typeof(SingletonInputTrigger).IsAssignableFrom(type)) {
-					continue;
-				}
-
-				var trigger = InputEngine.RegisterTrigger(type, type.Name, null);
-
-				typeof(Info<>).MakeGenericType(type)
-					.GetField(nameof(Info<SingletonInputTrigger>.id), BindingFlags.Static | BindingFlags.Public)
-					.SetValue(null, trigger.Id);
-			}
 		}
 	}
 }
