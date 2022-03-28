fb88ab5910a78efea67eac581e4c2572627bd710
Mirsario <me@mirsar.io>
2022-03-28 18:03:59 +0300

    Improved the probably temporary SpawnEntityMessage.

--- Src/_Core/_ECS/_Entities/EntitySpawningSystem.cs
+++ Src/_Core/_ECS/_Entities/EntitySpawningSystem.cs
@@ -1,6 +1,4 @@
-﻿using Dissonance.Engine.IO;
-
-namespace Dissonance.Engine
+﻿namespace Dissonance.Engine
 {
 	[Callback<EndRenderUpdateCallback>]
 	[Callback<EndFixedUpdateCallback>]
@@ -9,8 +7,7 @@ namespace Dissonance.Engine
 		protected override void Execute()
 		{
 			foreach (var message in ReadMessages<SpawnEntityMessage>()) {
-				var prefab = Assets.Find<EntityPrefab>(message.PrefabName).GetValueImmediately();
-				var entity = prefab.Clone(World);
+				var entity = message.SourceEntity.Clone(World);
 
 				message.Action?.Invoke(entity);
 			}
--- Src/_Core/_ECS/_Entities/SpawnEntityMessage.cs
+++ Src/_Core/_ECS/_Entities/SpawnEntityMessage.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using Dissonance.Engine.IO;
 
 namespace Dissonance.Engine
 {
 	public readonly struct SpawnEntityMessage
 	{
-		public readonly string PrefabName;
+		public readonly Entity SourceEntity;
 		public readonly Action<Entity> Action;
 
 		public SpawnEntityMessage(string prefabAssetPath, Action<Entity> setupAction = null)
+			: this(Assets.Find<EntityPrefab>(prefabAssetPath).GetValueImmediately(), setupAction) { }
+
+		public SpawnEntityMessage(EntityPrefab prefab, Action<Entity> setupAction = null)
+			: this((Entity)prefab, setupAction) { }
+
+		public SpawnEntityMessage(Entity sourceEntity, Action<Entity> setupAction = null)
 		{
-			PrefabName = prefabAssetPath;
+			SourceEntity = sourceEntity;
 			Action = setupAction;
 		}
 	}
