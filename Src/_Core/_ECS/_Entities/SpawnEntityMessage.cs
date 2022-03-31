f3c0535023064f47e173814ef4b0fd562aa06563
Mirsario <me@mirsar.io>
2022-03-31 04:18:50 +0300

    Make SpawnEntityMessage store source prefabs on entities.

--- Src/_Core/_ECS/_Entities/SpawnEntityMessage.cs
+++ Src/_Core/_ECS/_Entities/SpawnEntityMessage.cs
@@ -5,19 +5,23 @@ namespace Dissonance.Engine
 {
 	public readonly struct SpawnEntityMessage
 	{
+		public static bool DefaultWritePrefab { get; set; } = true;
+
 		public readonly Entity SourceEntity;
 		public readonly Action<Entity> Action;
+		public readonly bool WritePrefab;
 
-		public SpawnEntityMessage(string prefabAssetPath, Action<Entity> setupAction = null)
-			: this(Assets.Find<EntityPrefab>(prefabAssetPath).GetValueImmediately(), setupAction) { }
+		public SpawnEntityMessage(string prefabAssetPath, Action<Entity> setupAction = null, bool? writePrefab = null)
+			: this(Assets.Find<EntityPrefab>(prefabAssetPath).GetValueImmediately(), setupAction, writePrefab) { }
 
-		public SpawnEntityMessage(EntityPrefab prefab, Action<Entity> setupAction = null)
-			: this((Entity)prefab, setupAction) { }
+		public SpawnEntityMessage(EntityPrefab prefab, Action<Entity> setupAction = null, bool? writePrefab = null)
+			: this((Entity)prefab, setupAction, writePrefab) { }
 
-		public SpawnEntityMessage(Entity sourceEntity, Action<Entity> setupAction = null)
+		public SpawnEntityMessage(Entity sourceEntity, Action<Entity> setupAction = null, bool? writePrefab = null)
 		{
 			SourceEntity = sourceEntity;
 			Action = setupAction;
+			WritePrefab = writePrefab ?? DefaultWritePrefab;
 		}
 	}
 }
