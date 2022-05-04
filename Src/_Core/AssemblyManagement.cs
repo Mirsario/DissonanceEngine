88abb5a05d120f579f4561b2b816bf0d0675fc3f
Mirsario <me@mirsar.io>
2022-05-05 02:45:51 +0300

    Reimplemented Assembly & Module management to be less convoluted.

--- Src/_Core/AssemblyManagement.cs
+++ Src/_Core/AssemblyManagement.cs
@@ -2,51 +2,67 @@
 using System.Collections.Generic;
 using System.Reflection;
 
+#nullable enable
+
 namespace Dissonance.Engine
 {
 	public static class AssemblyManagement
 	{
 		public delegate void AssemblyRegistrationCallback(Assembly assembly, Type[] types);
 
-		public static event AssemblyRegistrationCallback OnAssemblyRegistered;
+		private static readonly List<WeakReference<Assembly>> assemblies = new();
 
-		private static readonly List<WeakReference<Assembly>> Assemblies = new();
-
-		private static bool ready;
+		private static AssemblyRegistrationCallback? onAssemblyRegistered;
+		
+		static AssemblyManagement()
+		{
+			RegisterAssembly(typeof(AssemblyManagement).Assembly);
+		}
 
 		public static void RegisterAssembly(Assembly assembly)
 		{
-			if (ready) {
-				OnAssemblyRegistered?.Invoke(assembly, assembly.GetTypes());
-			} else {
-				Assemblies.Add(new WeakReference<Assembly>(assembly));
+			lock (assemblies) {
+				onAssemblyRegistered?.Invoke(assembly, assembly.GetTypes());
+
+				assemblies.Add(new WeakReference<Assembly>(assembly));
 			}
+		}
+
+		/// <summary>
+		/// Registers a callback to be invoked for every future registered assembly, and also every assembly that has already been registered.
+		/// </summary>
+		/// <param name="callback"> The callback to execute for every assembly that will be or has been registered. </param>
+		public static void AddAssemblyCallback(AssemblyRegistrationCallback callback)
+		{
+			onAssemblyRegistered += callback;
+
+			foreach (var assembly in EnumerateAssemblies()) {
+				onAssemblyRegistered?.Invoke(assembly, assembly.GetTypes());
+			}
+		}
+
+		/// <summary>
+		/// Unregisters a
+		/// </summary>
+		/// <param name="callback"></param>
+		public static void RemoveAssemblyCallback(AssemblyRegistrationCallback callback)
+		{
+			onAssemblyRegistered -= callback;
 		}
 
 		public static IEnumerable<Assembly> EnumerateAssemblies()
 		{
-			foreach (var weakRef in Assemblies) {
-				if (weakRef.TryGetTarget(out var assembly)) {
+			lock (assemblies) {
+				for (int i = 0; i < assemblies.Count; i++) {
+					var weakRef = assemblies[i];
+
+					if (!weakRef.TryGetTarget(out var assembly)) {
+						assemblies.RemoveAt(i--);
+						continue;
+					}
+
 					yield return assembly;
 				}
-			}
-		}
-
-		internal static void Initialize()
-		{
-			RegisterAssembly(typeof(AssemblyManagement).Assembly);
-
-			ready = true;
-
-			for (int i = 0; i < Assemblies.Count; i++) {
-				var weakRef = Assemblies[i];
-
-				if (!weakRef.TryGetTarget(out var assembly)) {
-					Assemblies.RemoveAt(i--);
-					continue;
-				}
-
-				OnAssemblyRegistered?.Invoke(assembly, assembly.GetTypes());
 			}
 		}
 	}
